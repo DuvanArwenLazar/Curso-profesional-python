9416b9fa408678477bdb9500987683eba3df27f4
DuvanArwenLazar <3dtuningduvan@gmail.com>
2022-08-19 20:39:28 -0500

    Termino el programa.

--- palindrome.py
+++ palindrome.py
@@ -1,1 +1,16 @@
-# Programa.
+# Programa: Verificar si una cadena de caracteres es palindroma o no.
+
+# Funcion que hace la verificacion.
+def palindrome(cadena: str) -> bool:
+    # Reemplazamos los espacios vacios (" "), por una cadena vacia (""). Ademas, convertimos a minusculas.
+    cadena = cadena.replace(" ", "").lower()
+
+    # Le damos vuelva a un iterable (En este caso, la cadena) y lo comparamos con la cadena inicial. Esto devolvera un bool.
+    return cadena == cadena[::-1]
+
+# Funcion que correra el codigo principal
+def programa():
+    print(palindrome("ana"))
+
+if __name__ == '__main__':
+    programa()
