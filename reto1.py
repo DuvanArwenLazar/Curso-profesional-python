a41aaba9ac3fa9fc5525d334232f4429e0e0d6b9
DuvanArwenLazar <3dtuningduvan@gmail.com>
2022-08-20 07:25:56 -0500

    Ejercicio finalizado.

--- reto1.py
+++ reto1.py
@@ -1,37 +1,33 @@
-# Programa: Verificar si un numero es primo o no.
-# Funcion. Utilizaremos python como lenguaje de tipado "estatico".
-# from numbers import Integral
+# Programa: Verificar si un numero es primo o no. Utilizaremos python como lenguaje de tipado "estatico".
+n = int(input("-> Bienvenido \n -> Digite un numero: "))
 
-# a = 10
-# b = 5
-
-# hola = divmod(a, b)
-# print(hola)
-
+# Funcion: 
 def numeroPrimo(n: int) -> int:
     i: int = 0
     contador: int = 0
 
     while i <= n:
-        valor: int = divmod(n, i)
-        if(valor[1] == 0): 
+        # valor: int = divmod(n, i)
+        if(n%2== 0): 
             contador += 1
             '''Si la division del numero (que digita el usuario), por el iterador es igual a 0, entonces es un 
                 divisor del numero, por lo que se agrega al contador. '''
-    i+=1
 
-    if(contador > 2):
-        print(f"-> El numero {n} es primo.")
+        i+=1
+
+    if(n == 0 or n == 1):
+        print(f"-> El numero NO es primo ni compuesto")
+        
     else:
-        print(f"El numero {n} es compuesto")
+        if(n == 2):
+            print(f"-> El numero {n} es primo.")
+        else:
+            if(contador > 2):
+                print(f"-> El numero {n} es compuesto.")
+            else:
+                print(f"El numero {n} es primo.")
 
 if __name__ == '__main__':
-    n: int = input("-> Bienvenido \n -> Digite un numero: ")
-    
-    # Convertimos a entero la variable 'n'.
-    # int(n)
-
-    # Llamamos la funcion.
     numeroPrimo(n)
 
 
